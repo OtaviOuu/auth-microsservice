8f131ac65a9ecb742ba363d03ceca8cbc5fce648
OtaviOuu <conta.otaviou@gmail.com>
2024-11-23 23:20:23 -0300

    feat: login with email and pass

--- chalicelib/views/singup.py
+++ chalicelib/views/singup.py
@@ -1,11 +1,23 @@
 from chalice import Blueprint
 from chalicelib.utils.ssm import get_firebase_credential
 
+import json
+
+import pyrebase
+
 
 extra_routes_signup = Blueprint(__name__)
 creds = get_firebase_credential(parameter="firebase-client")
+firebase = pyrebase.initialize_app(json.loads(creds)).auth()
 
 
 @extra_routes_signup.route("/signup", methods=["POST"])
 def signup():
-    return {"hello": "world"}
+    try:
+        body = extra_routes_signup.current_request.json_body
+        user = firebase.create_user_with_email_and_password(
+            email=body.get("email"), password=body.get("password")
+        )
+        return user
+    except Exception as e:
+        return {"error": str(e)}
